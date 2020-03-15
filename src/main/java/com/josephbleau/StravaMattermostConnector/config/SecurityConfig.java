98319e9c83b375ad2a72b4a67697930a0cc44a3d
Joseph Bleau <jbleau@gmail.com>
2020-03-15 17:33:07 -0400

    Dont require authentication for our strava event hook endpoint

--- src/main/java/com/josephbleau/StravaMattermostConnector/config/SecurityConfig.java
+++ src/main/java/com/josephbleau/StravaMattermostConnector/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/login").permitAll()
+                    .antMatchers("/", "/login", "/strava/event").permitAll()
                     .antMatchers("/**").authenticated()
                 .and()
                 .oauth2Login()
